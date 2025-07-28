15c07579fbc8cea55bc7e37671aead6acc77a489
Haoming <hmstudy02@gmail.com>
2025-07-28 17:10:35 +0800

    html

--- modules/ui.py
+++ modules/ui.py
@@ -957,32 +957,31 @@ def create_ui():
 
 
 def versions_html():
+    from backend.memory_management import xformers_enabled  # sage_enabled, flash_enabled
+    import importlib.metadata
     import torch
-    import launch
 
     python_version = ".".join([str(x) for x in sys.version_info[0:3]])
-    commit = launch.commit_hash()
-    tag = launch.git_tag()
+    _versions = [f"torch: {getattr(torch, '__long_version__', torch.__version__)}"]
 
-    if shared.xformers_available:
-        import xformers
-        xformers_version = xformers.__version__
-    else:
-        xformers_version = "N/A"
+    # if sage_enabled():
+    #     _versions.append(f"sage: {importlib.metadata.version('sageattention')}")
+    # if flash_enabled():
+    #     _versions.append(f"flash: {importlib.metadata.version('flash_attn')}")
+    if xformers_enabled():
+        _versions.append(f"xformers: {importlib.metadata.version('xformers')}")
 
     return f"""
-version: <a href="https://github.com/lllyasviel/stable-diffusion-webui-forge/commit/{commit}">{tag}</a>
+version: <a href="https://github.com/Haoming02/sd-webui-forge-classic/tree/neo">neo</a>
 &#x2000;•&#x2000;
 python: <span title="{sys.version}">{python_version}</span>
 &#x2000;•&#x2000;
-torch: {getattr(torch, '__long_version__',torch.__version__)}
-&#x2000;•&#x2000;
-xformers: {xformers_version}
+{'&#x2000;•&#x2000;'.join(_versions)}
 &#x2000;•&#x2000;
 gradio: {gr.__version__}
 &#x2000;•&#x2000;
 checkpoint: <a id="sd_checkpoint_hash">N/A</a>
-"""
+            """
 
 
 def setup_ui_api(app):
