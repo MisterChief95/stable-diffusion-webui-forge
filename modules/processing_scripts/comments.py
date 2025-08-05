fe749365f7070d960c3cf24a9745880a8902b736
Haoming <hmstudy02@gmail.com>
2025-08-05 15:10:42 +0800

    scripts

--- modules/processing_scripts/comments.py
+++ modules/processing_scripts/comments.py
@@ -1,13 +1,19 @@
-from modules import shared
 import re
+
+from modules import shared
+
 
 def strip_comments(text):
     if shared.opts.enable_prompt_comments:
-        text = re.sub('(^|\n)#[^\n]*(\n|$)', '\n', text)    # whole line comment
-        text = re.sub('#[^\n]*(\n|$)', '\n', text)          # in the middle of the line comment
+        text = re.sub("(^|\n)#[^\n]*(\n|$)", "\n", text)  # whole line comment
+        text = re.sub("#[^\n]*(\n|$)", "\n", text)  # in the middle of the line comment
 
     return text
 
-shared.options_templates.update(shared.options_section(('sd', "Stable Diffusion", "sd"), {
-    "enable_prompt_comments": shared.OptionInfo(True, "Enable comments").info("Use # anywhere in the prompt to hide the text between # and the end of the line from the generation."),
-}))
+
+shared.options_templates.update(
+    shared.options_section(
+        ("ui_alternatives", "UI Alternatives", "ui"),
+        {"enable_prompt_comments": shared.OptionInfo(True, "Enable Comments").info("Ignore the texts between # and the end of the line from the prompts")},
+    )
+)
