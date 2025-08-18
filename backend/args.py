8371442c2b906ab9cd5ffa8cef82ca89b2a534ee
Brendan Lackey <brendan.lackey@outlook.com>
2025-08-18 16:52:29 -0700

    remove xformers_verbose flag

--- backend/args.py
+++ backend/args.py
@@ -40,7 +40,6 @@ upcast.add_argument("--disable-attention-upcast", action="store_true")
 parser.add_argument("--disable-xformers", action="store_true")
 parser.add_argument("--disable-xformers-attention", action="store_true")
 parser.add_argument("--disable-xformers-vae", action="store_true")
-parser.add_argument("--xformers-verbose", action="store_true", help="Enable xformers verbose mode.")
 
 parser.add_argument("--directml", type=int, nargs="?", metavar="DIRECTML_DEVICE", const=-1)
 parser.add_argument("--disable-ipex-hijack", action="store_true")
